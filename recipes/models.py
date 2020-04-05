88e83db4f49c945c8625d6eb481e5c6ceaedde01
Agustin <luquesagustin@gmail.com>
2020-04-05 17:01:24 -0300

    add recipe, ingredients, amount

--- recipes/models.py
+++ recipes/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from django.conf import settings
 
+class Weight(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '%s' % (self.name)       
+
+class Amount(models.Model):
+    unit = models.FloatField()
+    weight = models.ForeignKey(Weight, on_delete=models.CASCADE)
+        
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=300)
+    amount = models.ForeignKey(Amount, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s' % (self.name)
 
 class Recipe(models.Model):
     title = models.CharField(max_length=300)
     desc = models.CharField(max_length=300)
     image = models.ImageField(upload_to='images/%Y-%m-%d')
+    ingredients = models.ManyToManyField(Ingredient, related_name='recipes')
 
     def __str__(self):
         return '%s' % (self.title)
--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,24 @@ from .serializers import *
 from django.shortcuts import get_object_or_404
 from rest_framework.parsers import FileUploadParser
 
+class Weights(generics.ListCreateAPIView):
+    queryset = Weight.objects.all()
+    serializer_class = WeightSerializer
+
+class Weight(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Weight.objects.all()
+    serializer_class = WeightSerializer
+    lookup_field='pk'
+
+class Ingredients(generics.ListCreateAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+class Ingredient(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    lookup_field='pk'
+
 class Recipes(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
