ed9ef0b9603f76ca88d84ebcdc407ee93f9274ab
Martin Cura <martincura@gmail.com>
2020-07-07 11:35:52 -0300

    products: unit's short_name can be  null

--- src/apps/products/models.py
+++ src/apps/products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Unit(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    short_name = models.CharField(max_length=50, unique=True)
+    short_name = models.CharField(max_length=50, unique=True, null=True)
 
     def __str__(self):
         return self.name
