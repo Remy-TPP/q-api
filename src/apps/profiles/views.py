814626ab3090e1a3fa133c77eff692185235c5ea
Agustin <luquesagustin@gmail.com>
2020-07-12 19:39:54 -0300

    change comments for swagger

--- src/apps/profiles/views.py
+++ src/apps/profiles/views.py
@@ -3,8 +3,10 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
-
+from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
+from drf_yasg.utils import swagger_auto_schema
+
 from apps.profiles.models import (Profile,
                                   ProfileType,
                                   Group,
@@ -27,42 +29,38 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     lookup_field = 'pk'
 
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="Lists all profiles.",
+    operation_description="Returns profiles."
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_summary="Gets profile with id={id}.",
+    operation_description="Returns profile."
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_summary="Partial updates profile with id={id}",
+    operation_description="Returns profile. Only own profile."
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary="Updates profile with id={id}.",
+    operation_description="Returns profile. Only own profile."
+))
 class ProfileViewSet(viewsets.GenericViewSet,
                      mixins.ListModelMixin,
                      mixins.RetrieveModelMixin,
                      mixins.UpdateModelMixin):
-    """
-    Manage profiles
-
-    list: Lists all profiles.
-
-    Returns profiles.
-
-    retrieve: Gets profile with id={id}.
-
-    Returns profile.
-
-    partial_update: Partial updates profile with id={id}.
-
-    Returns profile. Only own profile.
-
-    update: Updates profile with id={id}.
-
-    Returns profile. Only own profile.
-    """
     queryset = Profile.objects.all().order_by("id")
     serializer_class = ProfileSerializer
     lookup_field = 'pk'
     permission_classes = [UpdateOwnProfile]
 
+    @swagger_auto_schema(
+        method='post',
+        operation_summary="Set profile's user is_active to False.",
+        operation_description="Only if the user of this profile is active"
+    )
     @action(detail=True, methods=['POST'], permission_classes=[IsOwnProfile])
     def inactivate(self, request, pk=None):
-        """
-        Set profile's user is_active to False.
-
-        Only if the user of this profile is active
-        """
         profile = get_object_or_404(Profile.objects.all(), id=pk)
 
         self.check_object_permissions(self.request, profile)
@@ -74,19 +72,17 @@ class ProfileViewSet(viewsets.GenericViewSet,
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response("The user is already inactive!", status=status.HTTP_400_BAD_REQUEST)
 
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="List all friendship requests.",
+    operation_description="Only where current user is involved."
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary="Create a friendship request.",
+    operation_description="From current user to user sent."
+))
 class FriendshipRequestViewSet(viewsets.GenericViewSet,
                                mixins.ListModelMixin,
                                mixins.CreateModelMixin):
-    """
-    list: List all friendship requests.
-
-    Only where current user is involved.
-
-    create: Create a friendship request.
-
-    From current user to user sent.
-    """
     queryset = FriendshipRequest.objects.all().order_by('id')
     serializer_class = FriendshipRequestSerializer
     lookup_field = 'pk'
@@ -97,13 +93,13 @@ class FriendshipRequestViewSet(viewsets.GenericViewSet,
             Q(profile_requested__user_id=self.request.user.id)
         ).order_by("id")
 
+    @swagger_auto_schema(
+        method='post',
+        operation_summary="Accept the request with {id}.",
+        operation_description="Only if the status is REQUESTED and the profile_requested is the current user."
+    )
     @action(detail=True, methods=['POST'])
     def accept(self, request, pk=None):
-        """
-        Accept the request with {id}.
-
-        Only if the status is REQUESTED and the profile_requested is the current user.
-        """
         friendship_request = get_object_or_404(FriendshipRequest.objects.all(), id=pk)
         status_requested = get_object_or_404(FriendshipStatus, name='REQUESTED')
         status_accepted = get_object_or_404(FriendshipStatus, name='ACCEPTED')
@@ -117,13 +113,13 @@ class FriendshipRequestViewSet(viewsets.GenericViewSet,
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response("Cannot add friend!", status=status.HTTP_400_BAD_REQUEST)
 
+    @swagger_auto_schema(
+        method='post',
+        operation_summary="Delete the request with {id}.",
+        operation_description="Only if the status is REQUESTED."
+    )
     @action(detail=True, methods=['POST'])
     def reject(self, request, pk=None):
-        """
-        Delete the request with {id}.
-
-        Only if the status is REQUESTED.
-        """
         friendship_request = get_object_or_404(FriendshipRequest.objects.all(), id=pk)
         status_requested = get_object_or_404(FriendshipStatus, name='REQUESTED')
         status_rejected = get_object_or_404(FriendshipStatus, name='REJECTED')
@@ -135,105 +131,102 @@ class FriendshipRequestViewSet(viewsets.GenericViewSet,
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response("Cannot delete friendship request!", status=status.HTTP_400_BAD_REQUEST)
 
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="Lists all profiletypes.",
+    operation_description="Returns profiletypes."
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_summary="Gets profiletype with id={id}..",
+    operation_description="Returns profiletype."
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary="Creates profiletype.",
+    operation_description="Returns profiletype."
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_summary="Partial updates profiletype with id={id}.",
+    operation_description="Returns profiletype."
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary="Updates profiletype with id={id}.",
+    operation_description="Returns profiletype."
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_summary="Deletes profiletype with id={id}.",
+    operation_description="Returns none."
+))
 class ProfileTypeViewSet(viewsets.ModelViewSet):
     """
     Manage the types of profiles. (Vegan, Celiac, etc.).
 
     Only Admin.
-
-    list: Lists all profiletypes.
-
-    Returns profiletypes.
-
-    retrieve: Gets profiletype with id={id}.
-
-    Returns profiletype.
-
-    create: Creates profiletype.
-
-    Returns profiletype.
-
-    partial_update: Partial updates profiletype with id={id}.
-
-    Returns profiletype.
-
-    update: Updates profiletype with id={id}.
-
-    Returns profiletype.
-
-    delete: Deletes profiletype with id={id}.
-
-    Returns none.
     """
     queryset = ProfileType.objects.all().order_by("id")
     serializer_class = ProfileTypeSerializer
     lookup_field = 'pk'
     permission_classes = [permissions.IsAdminUser]
 
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="Lists all groups.",
+    operation_description="Returns groups."
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_summary="Gets group with id={id}..",
+    operation_description="Returns group."
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary="Creates group.",
+    operation_description="Returns group."
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_summary="Partial updates group with id={id}.",
+    operation_description="Returns group."
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary="Updates group with id={id}.",
+    operation_description="Returns group."
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_summary="Deletes group with id={id}.",
+    operation_description="Returns none."
+))
 class GroupViewSet(viewsets.ModelViewSet):
     """
     Manage the groups of profiles.
-
-    list: Lists all groups.
-
-    Returns groups.
-
-    retrieve: Gets group with id={id}.
-
-    Returns group.
-
-    create: Creates group.
-
-    Returns group.
-
-    partial_update: Partial updates group with id={id}.
-
-    Returns group.
-
-    update: Updates group with id={id}.
-
-    Returns group.
-
-    delete: Deletes group with id={id}.
-
-    Returns none.
     """
     queryset = Group.objects.all().order_by("id")
     serializer_class = GroupSerializer
     lookup_field = 'pk'
 
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="Lists all friendshipstatus.",
+    operation_description="Returns friendshipstatus."
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_summary="Gets friendshipstatus with id={id}..",
+    operation_description="Returns friendshipstatus."
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary="Creates friendshipstatus.",
+    operation_description="Returns friendshipstatus."
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_summary="Partial updates friendshipstatus with id={id}.",
+    operation_description="Returns friendshipstatus."
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary="Updates friendshipstatus with id={id}.",
+    operation_description="Returns friendshipstatus."
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_summary="Deletes friendshipstatus with id={id}.",
+    operation_description="Returns none."
+))
 class FriendshipStatusViewSet(viewsets.ModelViewSet):
     """
     Manage the friendshipstatus of profiles.
 
     Only Admin.
-
-    list: Lists all friendshipstatus.
-
-    Returns friendshipstatus.
-
-    retrieve: Gets friendshipstatus with id={id}.
-
-    Returns friendshipstatus.
-
-    create: Creates friendshipstatus.
-
-    Returns friendshipstatus.
-
-    partial_update: Partial updates friendshipstatus with id={id}.
-
-    Returns friendshipstatus.
-
-    update: Updates friendshipstatus with id={id}.
-
-    Returns friendshipstatus.
-
-    delete: Deletes friendshipstatus with id={id}.
-
-    Returns none.
     """
     queryset = FriendshipStatus.objects.all().order_by("id")
     serializer_class = FriendshipStatusSerializer
