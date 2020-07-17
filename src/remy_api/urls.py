33c7403808689507279b481fa9c937ba83250003
Martin Cura <martincura@gmail.com>
2020-07-17 19:03:23 -0300

    add favicon

--- src/remy_api/urls.py
+++ src/remy_api/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
+
+favicon_view = RedirectView.as_view(url=f'{settings.MEDIA_URL}favicon_v2.ico')
 
 urlpatterns = [
+    re_path(r'^favicon\.ico$', favicon_view),
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.api.urls'), name='api'),
 ]
