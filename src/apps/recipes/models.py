0635b99f2dbcded18fc299ff9cec21e684581970
Martin Cura <martincura@gmail.com>
2020-07-20 15:54:17 -0300

    recipes: continue modeling

--- src/apps/recipes/models.py
+++ src/apps/recipes/models.py
@@ -1,28 +1,71 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 from apps.products.models import Amount, Product
 
 
+class DishCategory(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class DishLabel(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    # TODO: `related_name='+'` for either of categories or labels?
+    categories = models.ManyToManyField(DishCategory)
+    labels = models.ManyToManyField(DishLabel)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeInstructions(models.Model):
+    steps = ArrayField(models.TextField(), default=list)
+
+    def __str__(self):
+        """Example output: '[8 steps]'"""
+        return f"[{len(self.steps)} step{'s' if len(self.steps) != 1 else ''}]"
+
+    @classmethod
+    def default(cls):
+        return cls.objects.create().pk
+
+    @property
+    def displayable_steps(self):
+        """Example output: '1. Foo\n\n\n2. Bar\n\n\n3. Baz'"""
+        return '\n\n\n'.join([f'{i+1}. {t}' for i, t in enumerate(self.steps)])
+
+
 class Recipe(models.Model):
-    title = models.CharField(max_length=300)
+    dish = models.ForeignKey(Dish, on_delete=models.SET_NULL, null=True, related_name='recipes')
+    title = models.CharField(max_length=300, blank=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images/recipes/%Y-%m-%d', null=True)
     ingredients = models.ManyToManyField(Product, through='Ingredient')
+    instructions = models.OneToOneField(RecipeInstructions,
+                                        default=RecipeInstructions.default,
+                                        on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.title if self.title else self.dish.title
 
 
 class Ingredient(models.Model):
-    # TODO: add related_name='+' to product's FK?
+    # TODO: add related_name='+' to product's FK? Verify on_delete for product
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, null=True)
     amount = models.OneToOneField(Amount, on_delete=models.CASCADE)
-    # substitutions = models.ForeignKey('self',
-    #                                   blank=True,
-    #                                   null=True,
-    #                                   related_name='substitutions',
-    #                                   on_delete=models.CASCADE)
 
     def __str__(self):
         # TODO: make more human (e.g. '2 apples' instead of '2.00 of apple')
