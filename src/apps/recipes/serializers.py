b60d3b6d225bfe8f006ad997dc202a6f4cd35d8d
Martin Cura <martincura@gmail.com>
2020-07-22 14:16:09 -0300

    (wip) recipes: new serializers

--- src/apps/recipes/serializers.py
+++ src/apps/recipes/serializers.py
@@ -1,55 +1,132 @@
 from rest_framework import serializers
-from rest_framework_recursive.fields import RecursiveField
+# from rest_framework_recursive.fields import RecursiveField
 
-from apps.recipes.models import (Ingredient,
-                                 Recipe)
-from apps.products.models import Product
-from apps.products.serializers import AmountSerializer
+from apps.recipes.models import DishCategory, DishLabel, Dish, Ingredient, Recipe
+# from apps.products.models import Product
+from apps.products.serializers import AmountSerializer, ProductSerializer
+
+
+class DishCategorySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = DishCategory
+        fields = '__all__'
+
+
+class DishLabelSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = DishLabel
+        fields = '__all__'
+
+
+class DishSerializer(serializers.HyperlinkedModelSerializer):
+    categories = DishCategorySerializer(many=True)
+    labels = DishLabelSerializer(many=True)
+    recipes = serializers.HyperlinkedRelatedField(view_name='recipe-detail', many=True, read_only=True)
+
+    class Meta:
+        model = Dish
+        fields = '__all__'
+
+
+class RecipeMinimalSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = ['url', 'title']
 
 
 class IngredientSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    recipe = RecipeMinimalSerializer()
     amount = AmountSerializer()
-    product_id = serializers.PrimaryKeyRelatedField(
-        write_only=True,
-        source='product',
-        queryset=Product.objects.all()
-    )
-    product = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    substitutions = RecursiveField(allow_null=True, many=True)
+    # substitutions = RecursiveField(allow_null=True, many=True)
 
     class Meta:
         model = Ingredient
-        fields = ['id', 'amount', 'product', 'product_id', 'substitutions']
+        exclude = ['id']
 
-    # def get_fields(self):
-    #     fields = super(IngredientSerializer, self).get_fields()
-    #     fields['substitutions'] = IngredientSerializer(many=True)
-    #     return fields
+# TODO: recheck and clean up
+#     # def get_fields(self):
+#     #     fields = super(IngredientSerializer, self).get_fields()
+#     #     fields['substitutions'] = IngredientSerializer(many=True)
+#     #     return fields
+#
+#     def create(self, validated_data):
+#         amount_serializer = self.fields['amount']
+#         amount = amount_serializer.create(validated_data.pop('amount'))
+#         validated_data['amount'] = amount
+#
+#         # substitutions_serializer = self.fields['substitutions'].proxied
+#         # substitutions = substitutions_serializer.create(validated_data.pop('substitutions'))
+#
+#         ingredient = Ingredient.objects.create(**validated_data)
+#         # ingredient.substitutions.set(substitutions)
+#         return ingredient
 
-    def create(self, validated_data):
-        amount_serializer = self.fields['amount']
-        amount = amount_serializer.create(validated_data.pop('amount'))
-        validated_data['amount'] = amount
 
-        substitutions_serializer = self.fields['substitutions'].proxied
-        substitutions = substitutions_serializer.create(validated_data.pop('substitutions'))
+# class Ingredient2Serializer(serializers.HyperlinkedModelSerializer):
+#     id = serializers.ReadOnlyField(source='recipe.id')
+#     name = serializers.ReadOnlyField(source='recipe.name')
+#     amount = AmountSerializer()
+#
+#     class Meta:
+#         model = Ingredient
+#         fields = '__all__'
 
-        ingredient = Ingredient.objects.create(**validated_data)
-        ingredient.substitutions.set(substitutions)
-        return ingredient
+
+# class IngredientProductSerializer(serializers.HyperlinkedModelSerializer):
+#     recipes = Ingredient2Serializer(source='ingredient_set', many=True)
+#
+#     class Meta:
+#         model = Product
+#         fields = '__all__'
+#
+#
+# class Ingredient3Serializer(serializers.ModelSerializer):
+#     product = ProductSerializer()
+#
+#     class Meta:
+#         model = Ingredient
+#         # fields = ['amount']
+#         fields = '__all__'
+#         # queryset = Ingredient.objects.all()
+
+
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    amount = AmountSerializer()
+
+    class Meta:
+        model = Ingredient
+        exclude = ['id', 'recipe']
 
 
 class RecipeSerializer(serializers.HyperlinkedModelSerializer):
     image = serializers.ImageField(max_length=None, use_url=False, allow_null=True)
-    ingredients = IngredientSerializer(many=True)
+    instructions = serializers.SlugRelatedField(slug_field='steps', read_only=True)
+    # ingredients = IngredientSerializer(many=True, read_only=True)
+    # ingredients = ProductSerializer(many=True, read_only=True)
+    # ingredients = IngredientProductSerializer(many=True, read_only=True)
+    # filter()
+    # ingredients = Ingredient3Serializer(many=True, read_only=True)
+    # ingredientss = IngredientSerializer(source='ingredients', many=True, read_only=True)
+    ingredients = serializers.SerializerMethodField(method_name='get_ingredients')
 
     class Meta:
         model = Recipe
         fields = '__all__'
 
-    def create(self, validated_data):
-        ingredients_serializer = self.fields['ingredients']
-        ingredients = ingredients_serializer.create(validated_data.pop('ingredients'))
-        recipe = Recipe.objects.create(**validated_data)
-        recipe.ingredients.set(ingredients)
-        return recipe
+    def get_ingredients(self, obj):
+        return RecipeIngredientSerializer(
+            Ingredient.objects.filter(recipe=obj), many=True, context=self.context
+        ).data
+
+    # TODO: recheck and clean up
+    # def create(self, validated_data):
+    #     ingredients_serializer = IngredientSerializer(many=True, read_only=True)
+    #     ingredients = ingredients_serializer.create(validated_data.pop('ingredients'))
+    #     recipe = Recipe.objects.create(**validated_data)
+    #     recipe.ingredients.set(ingredients)
+    #     return recipe
