1745565a0e39b52d6b10dc6da6699b64471b7fae
Martin Cura <martincura@gmail.com>
2020-07-22 19:57:14 -0300

    products: change views' swagger doc style to same as in PR #7

--- src/apps/products/views.py
+++ src/apps/products/views.py
@@ -1,38 +1,40 @@
 from rest_framework import viewsets, permissions
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 
 from apps.products.models import Unit, Product
 from apps.products.serializers import UnitSerializer, ProductSerializer
 
 
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="Lists all units.",
+    operation_description="Returns units."
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_summary="Gets unit with id={id}.",
+    operation_description="Returns unit."
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary="Creates unit.",
+    operation_description="Returns unit."
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_summary="Partial updates unit with id={id}.",
+    operation_description="Returns unit."
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary="Updates unit with id={id}.",
+    operation_description="Returns unit."
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_summary="Deletes unit with id={id}.",
+    operation_description="Returns none."
+))
 class UnitViewSet(viewsets.ModelViewSet):
     """
     Manage units.
 
     Only Admin.
-
-    list: Lists all units.
-
-    Returns units.
-
-    retrieve: Gets unit with id={id}.
-
-    Returns unit.
-
-    create: Creates unit.
-
-    Returns unit.
-
-    partial_update: Partial updates unit with id={id}.
-
-    Returns unit.
-
-    update: Updates unit with id={id}.
-
-    Returns unit.
-
-    delete: Deletes unit with id={id}.
-
-    Returns none.
     """
     queryset = Unit.objects.all().order_by("id")
     serializer_class = UnitSerializer
@@ -40,35 +42,35 @@ class UnitViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
 
 
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="Lists all products.",
+    operation_description="Returns products."
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_summary="Gets product with id={id}.",
+    operation_description="Returns product."
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary="Creates product.",
+    operation_description="Returns product."
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_summary="Partial updates product with id={id}.",
+    operation_description="Returns product."
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary="Updates product with id={id}.",
+    operation_description="Returns product."
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_summary="Deletes product with id={id}.",
+    operation_description="Returns none."
+))
 class ProductViewSet(viewsets.ModelViewSet):
     """
     Manage products.
 
     Only Admin.
-
-    list: Lists all products.
-
-    Returns products.
-
-    retrieve: Gets product with id={id}.
-
-    Returns product.
-
-    create: Creates product.
-
-    Returns product.
-
-    partial_update: Partial updates product with id={id}.
-
-    Returns product.
-
-    update: Updates product with id={id}.
-
-    Returns product.
-
-    delete: Deletes product with id={id}.
-
-    Returns none.
     """
     queryset = Product.objects.all().order_by("id")
     serializer_class = ProductSerializer
