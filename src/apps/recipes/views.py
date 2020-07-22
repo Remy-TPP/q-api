76917789ae925ca82cbe09ad8c858643a2990fb0
Martin Cura <martincura@gmail.com>
2020-07-22 19:57:28 -0300

    recipes: change views' swagger doc style to same as in PR #7

--- src/apps/recipes/views.py
+++ src/apps/recipes/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.parsers import FileUploadParser
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 
 from apps.recipes.models import DishCategory, DishLabel, Dish, Ingredient, Recipe
 from apps.recipes.serializers import (DishCategorySerializer, DishLabelSerializer, DishSerializer,
@@ -28,67 +30,62 @@ class DishViewSet(viewsets.ModelViewSet):
 
 
 # TODO: IngredientViewSet is not needed, because we don't need ingredients/ endpoint... right?
+# (Because we could get/modify/create ingredients through the recipes/ endpoint)
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="Lists all ingredients.",
+    operation_description="Returns ingredients."
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_summary="Gets ingredient with id={id}.",
+    operation_description="Returns ingredient."
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary="Creates ingredient.",
+    operation_description="Returns ingredient."
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_summary="Partial updates ingredient with id={id}.",
+    operation_description="Returns ingredient."
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary="Updates ingredient with id={id}.",
+    operation_description="Returns ingredient."
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_summary="Deletes ingredient with id={id}.",
+    operation_description="Returns none."
+))
 class IngredientViewSet(viewsets.ModelViewSet):
-    """
-    Manage ingredients.
-
-    list: Lists all ingredients.
-
-    Returns ingredients.
-
-    retrieve: Gets ingredient with id={id}.
-
-    Returns ingredient.
-
-    create: Creates ingredient.
-
-    Returns ingredient.
-
-    partial_update: Partial updates ingredient with id={id}.
-
-    Returns ingredient.
-
-    update: Updates ingredient with id={id}.
-
-    Returns ingredient.
-
-    delete: Deletes ingredient with id={id}.
-
-    Returns none.
-    """
     queryset = Ingredient.objects.all().order_by("id")
     serializer_class = IngredientSerializer
     lookup_field = 'pk'
 
 
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary="Lists all recipes.",
+    operation_description="Returns recipes."
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_summary="Gets recipe with id={id}.",
+    operation_description="Returns recipe."
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary="Creates recipe.",
+    operation_description="Returns recipe."
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_summary="Partial updates recipe with id={id}.",
+    operation_description="Returns recipe."
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary="Updates recipe with id={id}.",
+    operation_description="Returns recipe."
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_summary="Deletes recipe with id={id}.",
+    operation_description="Returns none."
+))
 class RecipeViewSet(viewsets.ModelViewSet):
-    """
-    Manage recipes.
-
-    list: Lists all recipes.
-
-    Returns recipes.
-
-    retrieve: Gets recipe with id={id}.
-
-    Returns recipe.
-
-    create: Creates recipe.
-
-    Returns recipe.
-
-    partial_update: Partial updates recipe with id={id}.
-
-    Returns recipe.
-
-    update: Updates recipe with id={id}.
-
-    Returns recipe.
-
-    delete: Deletes recipe with id={id}.
-
-    Returns none.
-    """
     queryset = Recipe.objects.all().order_by("id")
     serializer_class = RecipeSerializer
     lookup_field = 'pk'
