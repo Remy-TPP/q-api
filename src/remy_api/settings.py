f848eeedfde0ab0cc96fcfd7fb79004138c46e65
Martin Cura <martincura@gmail.com>
2020-08-05 11:11:05 -0300

    Revert bf587d5: default permission is back to IsAuthenticated
    make this change later on and fix tests broken by it

--- src/remy_api/settings.py
+++ src/remy_api/settings.py
@@ -120,7 +120,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.IsAuthenticated',
+        # TODO: change to
+        # 'rest_framework.permissions.IsAdminUser',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
