c7dc093e0f2a845039601d863256c123428b8765
Martin Cura <martincura@gmail.com>
2020-08-06 16:54:06 -0300

    recipes: add section char field to Ingredient

--- src/apps/recipes/models.py
+++ src/apps/recipes/models.py
@@ -79,6 +79,7 @@ class Ingredient(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     amount = models.OneToOneField(Amount, on_delete=models.CASCADE, null=True)
+    section = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f'{self.amount} {self.product}'
