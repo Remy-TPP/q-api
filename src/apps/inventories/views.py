7c1d8765b8d548ea45b27abffc5f97d07448d8c6
Agustin <luquesagustin@gmail.com>
2021-01-31 11:22:29 -0300

    order inventory by product name

--- src/apps/inventories/views.py
+++ src/apps/inventories/views.py
@@ -110,7 +110,7 @@ class InventoryItemViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         place = get_place_or_default(self.request.user.profile, self.request.query_params.get('place'))
-        return self.filter_queryset(InventoryItem.objects.filter(place=place).order_by('id'))
+        return self.filter_queryset(InventoryItem.objects.filter(place=place).order_by('product__name'))
 
     @swagger_auto_schema(
         operation_summary="Create an item for that place.",
